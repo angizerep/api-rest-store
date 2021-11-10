c57ad6c4c03aa9faefaa66dc11db6da80c94a4b7
AngelesPerez <mperez@labsxd.com>
2021-11-10 09:24:14 -0400

    Refactorización de conexciones

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,30 @@
 'use strict'
 
+const User = require('../models/user')
+const service = require('../services')
+
+function singUp() {
+    const user = new User ({
+        name : req.body.name,
+        lastname : req.body.lastname,
+        displayName : req.body.displayName,
+        email : req.body.email,
+        avatar = req.body.avatar
+    })
+
+    user.save((err) => {
+        if (err) return res.status(500).send({message: `Error al registrar el usuario: ${err}`})
+        
+        res.status(200).send({ token: service.createToken })
+    })
+
+}
+
+function singIn (){
+
+}
+
+module.exports = {
+    singUp,
+    singIn
+}
