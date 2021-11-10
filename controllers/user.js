29c6e2a1f7523643cb142805d4668d7448999d34
AngelesPerez <mperez@labsxd.com>
2021-11-10 17:06:17 -0400

    Add login y register routers

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,13 @@
 const User = require('../models/user')
 const service = require('../services')
 
-function singUp() {
+function singUp( req, res ) {
     const user = new User ({
         name : req.body.name,
         lastname : req.body.lastname,
         displayName : req.body.displayName,
         email : req.body.email,
-        avatar = req.body.avatar
+        avatar : req.body.avatar
     })
 
     user.save((err) => {
@@ -20,8 +20,17 @@ function singUp() {
 
 }
 
-function singIn (){
+function singIn ( req, res ){
+    User.find({ email: req.body.email }, (err, user) => {
+        if (err) return res.status(500).send({message: err})
+        if (!user) return res.status(404).send({message: 'No existe el usuario'})
 
+        req.user = user
+        res.status(200).send({
+            message: 'Te has logueado correctamente',
+            token: service.createToken(user)
+        })
+    }) 
 }
 
 module.exports = {
--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,18 @@
 const express = require('express')
 const api = express.Router()
 const productController = require('../controllers/product')
+const auth = require('../middlewares/auth')
+const userController = require('../controllers/user')
 
 api.get('/product', productController.getAllProducts)
 api.get('/product/:productId', productController.getProductByID)
 api.post('/product', productController.createProducts)
 api.put('/product/:productId', productController.updateProduct)
 api.delete('/product/:productId', productController.deleteProduct)
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({message: 'Tienes acceso'})
+})
+api.post('/singup', userController.singUp)
+api.post('/singin', userController.singIn)
 
 module.exports = api
