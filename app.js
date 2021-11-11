be31492cb5b4677fe652f24531fa34ccb6e763dd
AngelesPerez <mperez@labsxd.com>
2021-11-11 15:29:45 -0400

    Configuración motor de plantilla

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 
 const express = require('express')
 const api = require('./routes/routes')
-
 const app = express()
+const hbs = require('express-handlebars')
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
 app.use('/api', api)
 
 module.exports = app
