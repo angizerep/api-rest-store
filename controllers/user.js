322dec7a83500c6dfa48f5855d193fa44fc908a0
Angeles <angi.zerep@gmail.com>
2021-11-25 22:37:56 -0400

    delete and inactivate user

--- controllers/user.js
+++ controllers/user.js
@@ -143,8 +143,140 @@ function changePassword ( req, res ){
     })
 }
 
+function inactivateUser ( req, res ){
+    
+    let isEqual = false;
+
+    User.findOne({ email: req.body.email }, (err, userFound) => { 
+        if (err) return res.status(500).send({message: err})
+        if (!userFound) return res.status(404).send({message: 'No existe el usuario'})
+        else {
+            console.log('userFound ', userFound)
+
+            UserPasswordHistory.find({ 
+                user: userFound._id
+            }, (err, userPasswordFound) => { 
+                if (err) return res.status(500).send({message: err})
+                if (!userPasswordFound) return res.status(404).send({message: 'No existe el usuario'})
+                else {
+
+                    let userPassword = '$2a$10$6YrIcYuwV1dBWIiB/Em3WOzFVBlxAQGhxhjjqZ6CdUgmDs5s42K72' //req.body.password
+                    console.log('userPassword ',userPassword)
+
+
+                    console.log('userPasswordFound ', userPasswordFound)
+                    console.log('userPasswordFound leng ', userPasswordFound.lenght)
+
+                    async.mapSeries( userPasswordFound, function(element , callback){
+                        console.log('element ',element)
+                        bcrypt.compare(req.body.password, element.password, function(err, resp) {
+                            // if (err){
+                            //     console.log('ERR Primero')
+                            //     res.status(404).send({message: 'passwords do not match'})
+                            // }
+                            if (resp){
+                              // Send JWT
+                                console.log('IF ', resp)
+                                // res.status(200).send({
+                                //     message: 'Te has logueado correctamente',
+                                //     token: service.createToken(userFound)
+                                // })
+                                isEqual = true;
+                                return callback;
+                            } else {
+                                isEqual = false;
+                                console.log('Else ', resp)
+                                console.log('Error ', err)
+                                return callback;
+                                // res.status(404).send({message: 'passwords do not match'})
+                            }
+                        });
+                    } )
+
+                    if (callback){
+                        if ( isEqual === true ) {
+                            res.status(404).send({message: 'MAL'})
+                        }
+                        else{
+                            console.log('Todo normal')
+                            res.status(200).send({message: 'BIEEEN'})
+                        }
+                    }
+                }
+            })
+        }
+    })
+}
+
+function deleteUser ( req, res ){
+    
+    let isEqual = false;
+
+    User.findOne({ email: req.body.email }, (err, userFound) => { 
+        if (err) return res.status(500).send({message: err})
+        if (!userFound) return res.status(404).send({message: 'No existe el usuario'})
+        else {
+            console.log('userFound ', userFound)
+
+            UserPasswordHistory.find({ 
+                user: userFound._id
+            }, (err, userPasswordFound) => { 
+                if (err) return res.status(500).send({message: err})
+                if (!userPasswordFound) return res.status(404).send({message: 'No existe el usuario'})
+                else {
+
+                    let userPassword = '$2a$10$6YrIcYuwV1dBWIiB/Em3WOzFVBlxAQGhxhjjqZ6CdUgmDs5s42K72' //req.body.password
+                    console.log('userPassword ',userPassword)
+
+
+                    console.log('userPasswordFound ', userPasswordFound)
+                    console.log('userPasswordFound leng ', userPasswordFound.lenght)
+
+                    async.mapSeries( userPasswordFound, function(element , callback){
+                        console.log('element ',element)
+                        bcrypt.compare(req.body.password, element.password, function(err, resp) {
+                            // if (err){
+                            //     console.log('ERR Primero')
+                            //     res.status(404).send({message: 'passwords do not match'})
+                            // }
+                            if (resp){
+                              // Send JWT
+                                console.log('IF ', resp)
+                                // res.status(200).send({
+                                //     message: 'Te has logueado correctamente',
+                                //     token: service.createToken(userFound)
+                                // })
+                                isEqual = true;
+                                return callback;
+                            } else {
+                                isEqual = false;
+                                console.log('Else ', resp)
+                                console.log('Error ', err)
+                                return callback;
+                                // res.status(404).send({message: 'passwords do not match'})
+                            }
+                        });
+                    } )
+
+                    if (callback){
+                        if ( isEqual === true ) {
+                            res.status(404).send({message: 'MAL'})
+                        }
+                        else{
+                            console.log('Todo normal')
+                            res.status(200).send({message: 'BIEEEN'})
+                        }
+                    }
+                }
+            })
+        }
+    })
+}
+
 module.exports = {
     changePassword,
     singUp,
-    singIn
+    singIn,
+    inactivateUser,
+    deleteUser
 }
